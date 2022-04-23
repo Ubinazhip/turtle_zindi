cdf3aeb180f898093d462ed486e55dc414110329
Ubingazhibov <36896850+Ubinazhip@users.noreply.github.com>
2022-04-23 14:15:00 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ python3 train.py --model_type b6 --batch_size 4 --epochs 50 --fold 3
 python3 train.py --model_type b6 --batch_size 4 --epochs 50 --fold 4
 
 
-CUDA_VISIBLE_DEVICES=2,3 python3 train.py --model_type b7 --batch_size 4 --epochs 50 --fold 0
+python3 train.py --model_type b7 --batch_size 4 --epochs 50 --fold 0
 
 #CUDA_VISIBLE_DEVICES=0,1 python3 inference.py --model_type b8 --batch_size 16 --num_classes 101 --model_path /home/ibm_prod/breast/model/backbone/code/b8_fold3_sampler.pth
 
